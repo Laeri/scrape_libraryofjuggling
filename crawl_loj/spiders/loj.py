d701e72f74ca485a93b55845b66b7d15c2264439
Laeri <lars.wuethrich@hotmail.com>
2019-11-15 17:02:58 +0100

    add #balls, show variation parent, scrape all tricks
    On second column we add the number of balls a trick requires before
    the defficulty. If the trick is a variation (it has a parent) we
    show this parent before the trick [<Parent>]<Trick>
    In addition we know also parse variations, which weren't parsed
    before at all!

--- crawl_loj/spiders/loj.py
+++ crawl_loj/spiders/loj.py
@@ -13,27 +13,45 @@ class LojSpider(scrapy.Spider):
     trick_data = []
     csv_file = open('juggling_tricks.csv', 'w')
     csv_writer = csv.writer(csv_file)
-    csv_writer.writerows([['Name', 'Difficulty', 'Prerequisites']])
+    csv_writer.writerows([['Name', 'NumBalls', 'Difficulty', 'Prerequisites']])
 
     def parse(self, response):
         # a panel that contains links to all available juggling tricks
-        trick_links = response.xpath('//div[@id="tree"]/ul[2]/li/ul/li')
-        for link in trick_links:
-            href = link.xpath('a/@href')
-            url = response.urljoin(href.extract_first())
-            yield response.follow(url, callback=self.parse_trick_data)
-        print(self.trick_data)
+
+        base_path = response.xpath(f"/html/body/form/div/div[2]/div[2]/ul[2]")
+        print(base_path)
+        tricks_per_ball = base_path[0].xpath(f"(//ul)[2]/li")
+        print(tricks_per_ball)
+        for i, trick_list in enumerate(tricks_per_ball):
+            num_balls = i + 3
+            trick_links = trick_list.xpath('ul/li')
+            #print(trick_links)
+            for link in trick_links:
+                href = link.xpath('a/@href')
+                url = response.urljoin(href.extract_first())
+                yield response.follow(url, callback=self.parse_trick_data, meta={'num_balls': num_balls})
+
+                variations = link.xpath('ul/li/a/@href')
+                for trick_var in variations:
+                    var_url = response.urljoin(trick_var.get())
+                    parent_name = self.canonicalize_name(url.split('/')[-1].replace('.html', ''))
+                    yield response.follow(var_url, callback=self.parse_trick_data, meta={'num_balls': num_balls, 'parent_name': parent_name})
+
 
     # each juggling trick has an explanation site
     # we extract the difficulty and prerequisites (other tricks that should be learned before this one)
     def parse_trick_data(self, response):
+        num_balls = response.meta.get('num_balls')
+        parent_name = response.meta.get('parent_name')
         trick_data = TrickData()
+        trick_data.num_balls = num_balls
         trick_data.name = self.canonicalize_name(response.url.split('/')[-1].replace('.html', ''))
+        if parent_name:
+            trick_data.name = f"[{parent_name}]{trick_data.name}"
         trick_data.difficulty = response.xpath('//ul[@id="otherinfo"]/li')[1].xpath('.//text()')[1].extract().replace(' ', '')
         trick_data.prerequisites = map(lambda plink: self.canonicalize_name(plink),
                                        response.xpath('//ul[@id="otherinfo"]/li')[2].xpath('a/@href').extract())
-        data = [trick_data.name, trick_data.difficulty]
-        print(trick_data.prerequisites)
+        data = [trick_data.name, trick_data.num_balls, trick_data.difficulty]
         for p in trick_data.prerequisites:
             data.append(p)
         self.csv_writer.writerows([data])
@@ -44,5 +62,9 @@ class LojSpider(scrapy.Spider):
         return str.split('/')[-1].replace('.html', ''). replace('\'', '')
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        print('CLOSE')
         self.csv_file.close()
+
+
+if __name__ == "__main__":
+    from scrapy.cmdline import execute
+    execute()
